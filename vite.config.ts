a45036bc4672d69caf93d3fb04fbb40312017f82
SanchezsX -dev~ <113286578+SanchezsX@users.noreply.github.com>
2024-06-02 11:33:45 +0200

    rename folder

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Todo',
+  base: '/Todo-settings-thems/',
   plugins: [react()],
   resolve: {
     alias: {
