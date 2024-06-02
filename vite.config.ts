4bb0a25b985825e273a68d1925cc40ba8ab32091
SanchezsX -dev~ <113286578+SanchezsX@users.noreply.github.com>
2024-06-02 11:38:46 +0200

    -

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Todo-settings-thems/',
+  base: '/Todo-settings-thems',
   plugins: [react()],
   resolve: {
     alias: {
